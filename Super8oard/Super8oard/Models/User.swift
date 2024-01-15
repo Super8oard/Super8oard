652a42d26e536f5d72679ebe179e0ab229ff6263
JiHoon Kang <deferare@icloud.com>
2024-01-15 15:41:47 +0900

    User 모델 생성

---
+++ Super8oard/Super8oard/Models/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  Super8oard
+//
+//  Created by JiHoon K on 1/15/24.
+//
+
+import Foundation
+
+struct User {
+    var id: String
+    var password: String
+    var name: String
+    var phoneNumber: String
+    var email: String
+    var birthDate: String
+}

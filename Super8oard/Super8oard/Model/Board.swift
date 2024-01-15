2cd4a05ca52670269286aef50d334ad529e5b1c0
Darwin <limsj318@naver.com>
2024-01-15 16:06:31 +0900

    구조체 등록

---
+++ Super8oard/Super8oard/Model/Board.swift
@@ -0,0 +1,18 @@
+//
+//  Board.swift
+//  Super8oard
+//
+//  Created by 석진 on 1/15/24.
+//
+
+import Foundation
+
+
+struct Board
+{
+    var boardType: String
+    var boardNumber: Int
+    var boardBattery: Int
+    var boardPrice: Int
+    var boardLocation: Any
+}
--- Super8oard/Super8oard/ViewController.swift
+++ Super8oard/Super8oard/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let aaa = NMGLatLng(lat: )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

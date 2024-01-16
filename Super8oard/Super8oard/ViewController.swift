e60f57cd8330a6a38b3298bb1bf33b73e890b923
JiHoon Kang <deferare@icloud.com>
2024-01-16 11:18:16 +0900

    메인 뷰컨에서 로그인 안되있으면 로그인 화면 나오게 함

--- Super8oard/Super8oard/ViewController.swift
+++ Super8oard/Super8oard/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if !UserDefaults.standard.bool(forKey: "user") { // 로그인 필요
+            let logInVC = UIStoryboard(name: "LoginStoryboard", bundle: nil).instantiateViewController(withIdentifier: "LoginNaviViewController") as! UINavigationController
+            
+            present(logInVC, animated: true)
+        } else {
+            
+        }
+    }
 }
-
